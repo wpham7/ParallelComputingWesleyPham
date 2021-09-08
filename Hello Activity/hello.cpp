144bd515e50f3ce52b8712b90aaa437a3aab943a
wpham7 <wesleypham7.wp@gmail.com>
2021-09-08 14:40:42 -0400

    homework solution

--- Hello Activity/hello.cpp
+++ Hello Activity/hello.cpp
@@ -1,4 +1,10 @@
-int main () {
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
 
+int main () {
+  char hostname[HOST_NAME_MAX];
+  gethostname(hostname, HOST_NAME_MAX);
+  printf("Hostname is: %s \n", hostname);
   return 0;
 }
