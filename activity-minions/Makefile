273adce621656f26dac34788195c294f4423f288
wpham7 <wesleypham7.wp@gmail.com>
2021-10-07 22:50:08 -0400

    adding minions assignment

---
+++ activity-minions/Makefile
@@ -0,0 +1,18 @@
+CXXFLAGS=-std=c++17
+LDLIBS=-lpthread
+
+run: minions
+	./minions 40
+
+minions:
+
+activity-minions.pdf: activity-minions.tex
+	pdflatex $<
+
+FILESTOINCLUDE=Makefile activity-minions.pdf minions.cpp
+
+activity-minions.tgz: $(FILESTOINCLUDE)
+	tar zcvf $@ $(FILESTOINCLUDE)
+
+clean:
+	rm minions
---
+++ activity-minions/minions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <thread>
+
+void f (int a) {
+  std::cout<<"Hello! I am minion "<<a<<". \n";
+} 
+
+void writeoverlordmessage() {
+  std::cout<<"Hello Minions! I am the Overlord.\n";
+}
+int main (int argc, char** argv) {
+  if (argc < 2) {
+    std::cerr<<"usage: "<<argv[0]<<" <nbminions>\n";
+    return -1;
+  }
+  //Your code goes here
+  for (int i = 0; i < atoi(argv[1]); i++) {
+    std::thread minion_thread(f, (i+1));
+
+    minion_thread.join();
+  }
+
+  std::thread overlord_thread(writeoverlordmessage);
+  overlord_thread.join();
+  return 0;
+}
